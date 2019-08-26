acb454148b3de764f8a2114b0ea51358db798357
Jan Kuehle <jan.kuehle@red-badger.com>
2019-08-26 15:02:56 +0100

    Automate grafana configuration

--- grafana/deploy.sh
+++ grafana/deploy.sh
@@ -2,5 +2,27 @@
 
 set -e
 
-kyml cat infrastructure/* |
+configmap_provisioning_datasources() {
+    kubectl create configmap grafana-provisioning-datasources \
+        --dry-run \
+        --output yaml \
+        --from-file=prometheus.yml=./config/provisioning/datasources/prometheus.yml
+}
+
+configmap_provisioning_dashboards() {
+    kubectl create configmap grafana-provisioning-dashboards \
+        --dry-run \
+        --output yaml \
+        --from-file=files.yml=./config/provisioning/dashboards/files.yml
+}
+
+configmap_dashboards() {
+    kubectl create configmap grafana-dashboards \
+        --dry-run \
+        --output yaml \
+        --from-file=main.json=./config/dashboards/main.json
+}
+
+kyml cat infrastructure/* <(configmap_provisioning_datasources) <(configmap_provisioning_dashboards) <(configmap_dashboards) |
+    kyml resolve |
     kubectl apply -f -
